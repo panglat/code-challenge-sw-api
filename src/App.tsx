97939f98d65d78132dc675ef3e0524d5a80b099c
Pablo Anglat <pablo.anglat@globant.com>
2020-10-17 12:43:49 -0300

    Adding a Sass Stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
