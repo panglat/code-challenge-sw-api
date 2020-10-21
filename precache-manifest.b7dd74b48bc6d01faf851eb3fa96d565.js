662923d877cf569bd873384fdbe648d6727df867
panglat <panglat@gmail.com>
2020-10-21 05:06:27 +0000

    Updates

---
+++ precache-manifest.b7dd74b48bc6d01faf851eb3fa96d565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee2f83a5465b9705127f7d8c44cbce1",
+    "url": "/code-challenge-sw-api/index.html"
+  },
+  {
+    "revision": "ea46f735f4c6477305c7",
+    "url": "/code-challenge-sw-api/static/css/main.a0691e8c.chunk.css"
+  },
+  {
+    "revision": "9a719fc6f4af57daa9c0",
+    "url": "/code-challenge-sw-api/static/js/2.7f912a41.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/code-challenge-sw-api/static/js/2.7f912a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea46f735f4c6477305c7",
+    "url": "/code-challenge-sw-api/static/js/main.fa283065.chunk.js"
+  },
+  {
+    "revision": "56b8d8c18902b5cade97",
+    "url": "/code-challenge-sw-api/static/js/runtime-main.aa0ec410.js"
+  }
+]);
